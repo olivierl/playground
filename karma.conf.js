a020aa0077c86820bf310107af7e8468b61f80dd
olivierl <olivier.lambeaux@gmail.com>
2017-03-27 10:36:22 +0200

    add text summary coverage

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'cobertura'],
+      reports: ['html', 'text-summary', 'lcovonly', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     junitReporter: {
